01aa41004658d3bddd0bba5d1f3e1a787a0f46b9
alex_barmen <www.lad282@gmail.com>
2025-10-13 19:30:30 +0300

    feat: urls

--- db_project/db_project/urls.py
+++ db_project/db_project/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
