4cbd6843b14dc547648380fa5ab1eafdf2005b0d
alex_barmen <www.lad282@gmail.com>
2025-10-19 16:15:30 +0300

    feat: constants

---
+++ db_project/common_utils/constants.py
@@ -0,0 +1,13 @@
+MALE = 'male'
+FEMALE = 'female'
+GENDER_CHOICES = {
+    MALE: 'Мужской',
+    FEMALE: 'Женский',
+}
+
+SIMPLE = 'simple'
+FOREIGN = 'foreign'
+PASSPORT_TYPES = {
+    SIMPLE: 'Обычный',
+    FOREIGN: 'Заграничный',
+}
