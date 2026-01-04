3f0cb24c41f476c0edd2b65e5d4c1b9ac120687b
alex_barmen <www.lad282@gmail.com>
2026-01-04 17:46:36 +0300

    feat: admin for shops models

--- db_project/shops/admin.py
+++ db_project/shops/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
 
-# Register your models here.
+from shops.models import (
+    ProductUnit,
+    ProductCategory,
+    Product,
+    StorageProfile,
+    ProductMedia,
+    StorageType,
+    Storage,
+)
+
+@admin.register(ProductUnit)
+class ProductUnitAdmin(admin.ModelAdmin):
+    """Админ для ProductUnit"""
+
+    list_display = ['id', 'name']
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    """Админ для ProductCategory"""
+
+    list_display = ['id', 'name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админ для Product"""
+
+    list_display = ['id', 'name', 'unit', 'category', 'expiration_days', 'producer_name', 'country_name']
+    raw_id_fields = ['category', 'unit']
+    list_filter = ['category', 'unit']
+
+
+@admin.register(StorageProfile)
+class StorageProfileAdmin(admin.ModelAdmin):
+    """Админ для StorageProfile"""
+
+    list_display = ['id', 'product', 'weight', 'temp_min_c', 'temp_max_c', 'light_sensitive']
+    raw_id_fields = ['product']
+
+
+@admin.register(ProductMedia)
+class ProductMediaAdmin(admin.ModelAdmin):
+    """Админ для ProductMedia"""
+
+    list_display = ['id', 'product']
+    raw_id_fields = ['product']
+    list_filter = ['product']
+
+
+@admin.register(StorageType)
+class StorageTypeAdmin(admin.ModelAdmin):
+    """Админ для StorageType"""
+
+    list_display = ['id', 'name']
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    """Админ для Storage"""
+
+    list_display = [
+        'id',
+        'director',
+        'main_office_filial',
+        'storage_type',
+        'cadastral_number',
+        'opened',
+        'approved_by_main_company',
+        'area',
+    ]
+    raw_id_fields = ['director', 'main_office_filial']
+    list_filter = ['main_office_filial', 'opened']
--- db_project/shops/models.py
+++ db_project/shops/models.py
@@ -1,5 +1,3 @@
-from enum import unique
-
 from django.db import models
 
 from common_utils.mixins import AutoDateMixin
