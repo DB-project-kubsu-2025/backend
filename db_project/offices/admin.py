72fb57187c8019b09ea6d6e13c81660c2e395de2
alex_barmen <www.lad282@gmail.com>
2026-01-05 22:45:01 +0300

    feat: admin for offices

--- db_project/offices/admin.py
+++ db_project/offices/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from offices.models import (
+    Region,
+    City,
+    MainOfficeFilial,
+    StorageOpeningRequest,
+)
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    """Админ для Region"""
+
+    list_display = ['id', 'name']
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    """Админ для City"""
+
+    list_display = ['id', 'name', 'region']
+    raw_id_fields = ['region']
+    list_filter = ['region']
+
+
+@admin.register(MainOfficeFilial)
+class MainOfficeFilialAdmin(admin.ModelAdmin):
+    """Админ для MainOfficeFilial"""
+
+    list_display = ['id', 'city', 'director', 'address', 'cadastral_number']
+    raw_id_fields = ['city', 'director']
+    list_filter = ['city', 'director']
+
+
+@admin.register(StorageOpeningRequest)
+class StorageOpeningRequestAdmin(admin.ModelAdmin):
+    """Админ для StorageOpeningRequest"""
+
+    list_display = ['id', 'storage', 'main_office_filial', 'is_confirmed', 'confirmed_by', 'confirmation_date']
+    raw_id_fields = ['storage', 'main_office_filial', 'confirmed_by']
+    list_filter = ['main_office_filial', 'confirmed_by']
