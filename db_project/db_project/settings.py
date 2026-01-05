c5406ac377623d217547def939390195c2bfb99c
alex_barmen <www.lad282@gmail.com>
2026-01-05 19:50:43 +0300

    feat: update settings py

--- db_project/db_project/settings.py
+++ db_project/db_project/settings.py
@@ -11,12 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=True)
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='localhost')
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "app",
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='localhost')
 
 INSTALLED_APPS = [
     # local app
