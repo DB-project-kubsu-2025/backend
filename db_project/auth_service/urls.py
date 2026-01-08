15d24fc6e5b320ff4843bb5541404fae05b685d7
alex_barmen <www.lad282@gmail.com>
2026-01-08 12:21:37 +0300

    feat: change urls name

--- db_project/auth_service/urls.py
+++ db_project/auth_service/urls.py
@@ -13,8 +13,8 @@ from auth_service.views import (
 urlpatterns = [
     path('healthcheck/', HealthCheck.as_view(), name='health_check'),
     path('token/obtain/', CustomTokenObtainPairView.as_view(), name='token_obtain'),
-    path('employee/register/', RegisterEmployee.as_view(), name='Employee_register'),
-    path('employee/', EmployeeChange.as_view(), name='Employee_change'),
+    path('employee/register/', RegisterEmployee.as_view(), name='employee_register'),
+    path('employee/', EmployeeChange.as_view(), name='employee_change'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('password/change/', ChangePassword.as_view(), name='password_change'),
     path('logout/', LogOut.as_view(), name='log_out'),
