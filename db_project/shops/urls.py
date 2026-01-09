fd89244f8cb14f2a1932bac57e4a799a32ce57ab
alex_barmen <www.lad282@gmail.com>
2026-01-09 19:22:28 +0300

    fix: routing order

--- db_project/shops/urls.py
+++ db_project/shops/urls.py
@@ -46,8 +46,9 @@ shops_router = SimpleRouter()
 shops_router.register('products/units', ProductUnitViewSet)
 shops_router.register('products/categories', ProductCategoryViewSet)
 shops_router.register('products/media', ProductMediaViewSet)
-shops_router.register('products', ProductViewSet)
 shops_router.register('products/inventory_lots', ProductInventoryLotViewSet)
+shops_router.register('products', ProductViewSet)
+
 shops_router.register('storages/profiles', StorageProfileViewSet)
 shops_router.register('storages/types', StorageTypeViewSet)
 shops_router.register('storages/spaces/types', SpaceTypeViewSet)
@@ -56,31 +57,40 @@ shops_router.register('storages/inventory_balances', InventoryBalanceViewSet)
 shops_router.register('storages/inventory_movements', InventoryMovementViewSet)
 shops_router.register('storages/inventory_lots', InventoryLotViewSet)
 shops_router.register('storages', StorageViewSet)
+
 shops_router.register('movements/types', MovementTypeViewSet)
-shops_router.register('price-lists', PriceListViewSet)
+
 shops_router.register('price-lists/products', PriceListProductViewSet)
 shops_router.register('price-lists/types', PriceListTypeViewSet)
 shops_router.register('price-lists/bases', PriceListBaseViewSet)
+shops_router.register('price-lists', PriceListViewSet)
+
 shops_router.register('pricing/constraints', PricingConstraintViewSet)
 shops_router.register('pricing/runs', PricingRunViewSet)
 shops_router.register('pricing/store_prices', StorePriceViewSet)
-shops_router.register('coupons', CouponViewSet)
+
 shops_router.register('coupons/discounts/types', CouponDiscountTypeViewSet)
+shops_router.register('coupons', CouponViewSet)
+
 shops_router.register('sales/receipts', SaleReceiptViewSet)
 shops_router.register('sales/receipt_lines', SalesReceiptLineViewSet)
+
 shops_router.register('payments/methods', PaymentMethodViewSet)
-shops_router.register('stop-lists', StopListViewSet)
+
 shops_router.register('stop-lists/products', StopListProductViewSet)
 shops_router.register('stop-lists/reasons', StopListReasonViewSet)
-shops_router.register('stock-takes', StockTakeViewSet)
+shops_router.register('stop-lists', StopListViewSet)
+
 shops_router.register('stock-takes/lines', StockTakeLineViewSet)
 shops_router.register('stock-takes/adjustments', StockTakeAdjustmentViewSet)
 shops_router.register('stock-takes/types', StockTakeTypeViewSet)
-shops_router.register('writeoffs', WriteOffActViewSet)
+shops_router.register('stock-takes', StockTakeViewSet)
+
 shops_router.register('writeoffs/lines', WriteoffLineViewSet)
 shops_router.register('writeoffs/attachments', WriteoffAttachmentViewSet)
 shops_router.register('writeoffs/postings', WriteoffPostingViewSet)
 shops_router.register('writeoffs/reasons', WriteoffReasonViewSet)
+shops_router.register('writeoffs', WriteOffActViewSet)
 
 urlpatterns = [
     path('', include(shops_router.urls)),
