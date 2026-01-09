9e98f27441702b5abc457d993838f4d47a5f5e2a
QULVAR <127725086+QULVAR@users.noreply.github.com>
2026-01-10 01:37:17 +0300

    feature/documents approving (stage c)

--- db_project/supplies/urls.py
+++ db_project/supplies/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from supplies.views import (
+from supplies.views.private import (
     SupplierViewSet,
     SupplierContactViewSet,
     SupplyContractViewSet,
     SupplyContractProductViewSet,
+    
+    SupplyViewSet,
+    SupplyProductViewSet,
+    SupplyProductLotViewSet,
+    DiscrepancyReasonViewSet,
+    SupplyDiscrepancyViewSet,
+    DiscrepancyAttachmentViewSet,
 )
 
 router = SimpleRouter()
@@ -13,6 +20,12 @@ router.register("suppliers", SupplierViewSet)
 router.register("suppliers/contacts", SupplierContactViewSet)
 router.register("contracts", SupplyContractViewSet)
 router.register("contracts/products", SupplyContractProductViewSet)
+router.register("supplies", SupplyViewSet)
+router.register("supply-products", SupplyProductViewSet)
+router.register("supply-product-lots", SupplyProductLotViewSet)
+router.register("discrepancy-reasons", DiscrepancyReasonViewSet)
+router.register("discrepancies", SupplyDiscrepancyViewSet)
+router.register("discrepancy-attachments", DiscrepancyAttachmentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
